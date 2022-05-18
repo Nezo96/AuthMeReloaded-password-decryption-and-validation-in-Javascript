0685b47dee6e987d9647508d1c5ea31a0200bad8
Radoslav Rác <racradoslav96@gmail.com>
2022-05-18 09:31:52 +0200

    Added functionality to register new user

--- registerUser.js
+++ registerUser.js
@@ -1,7 +1,7 @@
 // Require sha256
 const saltedSha256 = require('salted-sha256');
 const mysql = require('mysql');
-const { inflateRaw } = require('pako');
+const fetch = require('node-fetch');
 
 require('dotenv').config()
 
@@ -16,24 +16,58 @@ const conn = mysql.createConnection({
 })
 
 // Function for register new user
-function registerUser(username, password, email) {
+async function registerUser(username, password, email) {
 
-    // Check if there is email or not
-    if ((email === "") || (email === undefined)) {
-        email = "your@domain.com";
-    } else {
-        email = email;
-    }
-    console.log(email);
+    //console.log(email);
+    // Make realname from username that we lowercase
+    const realname = username.toLowerCase();
+
+    // Wwait for function to getIP is done
+    const ip = await getIp();
+    //console.log(ip);
 
     // Connect to db and check for errors
     conn.connect((err) => {
         if (err) throw err;
         console.log('Connected to DB');
-    })
 
-    // Generate new password with given password and salt
-    //const newPassword = generateNewPassword(salt, password);
+        // Generate new password with given password and salt
+        const newPassword = generateNewPassword(salt, password);
+
+        //console.log('New password ' + newPassword);
+        // Inser constants to query sql string
+        const sql = `INSERT INTO authme (username, password, email, realname, ip) VALUES ('${username}', '${newPassword}', '${email}', '${realname}', '${ip}')`;
+
+        // Check if query sql is OK
+        conn.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log('User registered');
+        });
+    });
 }
 
-registerUser('Test', 'test');
+// Generate new password with our salt and given password
+function generateNewPassword(salt, password) {
+
+    // Hash given password with SHA256
+    const firstHash = saltedSha256(password);
+
+    // Hash given password with SHA256 again and add our salt
+    const secondHash = saltedSha256(firstHash, salt);
+
+    // Concatinate all together
+    const finalPassword = '$SHA$' + salt + '$' + secondHash;
+    //console.log(finalPassword);
+
+    return finalPassword;
+}
+
+// Function to get user IP
+function getIp() {
+    return fetch("https://api.ipify.org/?format=json")
+        .then(res => res.json())
+        .then(data => data.ip)
+        .catch((err) => console.log(err));
+}
+
+registerUser('TResesT', 'teste');
